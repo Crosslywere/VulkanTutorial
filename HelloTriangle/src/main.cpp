5578e87dcdd257334e45aed594e0688eb08d53f7
Crosslywere <ogboruj@outlook.com>
2024-12-12 19:02:40 +0100

    Updated includes

--- HelloTriangle/src/main.cpp
+++ HelloTriangle/src/main.cpp
@@ -1,7 +1,7 @@
 #if defined(_WIN32)
 #define VK_USE_PLATFORM_WIN32_KHR
 #endif
-#include <glm/\glm.hpp>
+#include <glm/glm.hpp>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
 #include <vulkan/vulkan.hpp>
