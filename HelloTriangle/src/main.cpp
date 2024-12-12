f666a3adb07c54098e6477855bb6d8cb437a399f
Crosslywere <ogboruj@outlook.com>
2024-12-12 18:47:48 +0100

    Created initial project file

---
+++ HelloTriangle/src/main.cpp
@@ -0,0 +1,11 @@
+#if defined(_WIN32)
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+#include <glm/\glm.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vulkan/vulkan.hpp>
+
+int main() {
+
+}
