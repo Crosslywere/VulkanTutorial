ea624a04d75f6c1cff68e47c47218730f44e6d9d
Crosslywere <ogboruj@outlook.com>
2024-12-13 15:02:35 +0100

    Added vpath to project

--- HelloTriangle/premake5.lua
+++ HelloTriangle/premake5.lua
@@ -2,6 +2,9 @@ project "HelloTriangle"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++17"
-	targetdir "bin/%{prj.name}/%{cfg.buildcfg}"
-	objdir "obj/%{prj.name}/%{cfg.buildcfg}"
+	targetdir "../bin/%{prj.name}/%{cfg.buildcfg}"
+	objdir "../obj/%{prj.name}/%{cfg.buildcfg}"
 	files {"**.cpp"}
+	vpaths {
+		["Source"] = "**.cpp"
+	}
