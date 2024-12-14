e7e14bd608e7cabaff553036b8bdf10076745f5a
Crosslywere <ogboruj@outlook.com>
2024-12-14 16:27:07 +0100

    moved filters from workspace to project premake file

--- HelloTriangle/premake5.lua
+++ HelloTriangle/premake5.lua
@@ -8,3 +8,17 @@ project "HelloTriangle"
 	vpaths {
 		["Source"] = "**.cpp"
 	}
+
+	filter "system:windows"
+		includedirs "$(VULKAN_SDK)/Include"
+		libdirs {"$(VULKAN_SDK)/Lib", "$(VULKAN_SDK)/Bin"}
+		links {"vulkan-1.lib", "SDL2.lib"}
+		defines "SDL_MAIN_HANDLED"
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		optimize "On"
